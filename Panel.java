4b100f8232688e9db63ed8186cee689de0ac92a2
kimcity <kiran@theganeshans.com>
2016-05-06 09:58:14 -0400

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -4,13 +4,28 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
-CurrentPanel currentPanel;
+CurrentPanel current;
+AlertsPanel alerts;
+TenDayPanel tenDay;
+DayPanel day;
+SearchPanel search;
 public class Panel extends JPanel
 {
    public Panel()
    {
       setLayout(new BorderLayout());
-      currentPanel = new CurrentPanel();
-      add(currentPanel, BorderLayout.NORTH);
+      current = new CurrentPanel();
+      add(current, BorderLayout.NORTH);
+      JPanel right = new JPanel();
+      day = new DayPanel();
+      alerts = new AlertsPanel();
+      search = new SearchPanel();
+      right.setLayout(new GridLayout(3,1));
+      right.add(search);
+      right.add(day);
+      right.add(alerts);
+      add(right, BorderLayout.EAST);
+      tenDay = new TenDayPanel();
+      add(tenDay,BorderLayout.SOUTH);
    }
 }
