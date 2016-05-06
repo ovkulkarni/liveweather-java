1172a23909d19dfe33bcacc0959605009c70b118
nikki2019 <awesomenikki1@gmail.com>
2016-05-06 09:50:00 -0400

    Added files via upload

---
+++ AlertsPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.image.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import org.json.*;
+import java.net.*;
+public class AlertsPanel extends JPanel 
+
+{
+      public AlertsPanel(){
+      
