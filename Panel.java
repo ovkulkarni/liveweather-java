d8d4a9f275822952f9a97c8b139305c650efb7c0
kimcity <kiran@theganeshans.com>
2016-05-06 09:49:59 -0400

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -4,11 +4,13 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
-
+CurrentPanel currentPanel;
 public class Panel extends JPanel
 {
    public Panel()
    {
-      add(new CurrentPanel());
+      setLayout(new BorderLayout());
+      currentPanel = new CurrentPanel();
+      add(currentPanel, BorderLayout.NORTH);
    }
 }
