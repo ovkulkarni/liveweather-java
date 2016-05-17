5d598b8507bccb35462c457721d854cc7e6625fa
kimcity <kiran@theganeshans.com>
2016-05-16 23:26:25 -0400

    Update SearchPanel.java

--- SearchPanel.java
+++ SearchPanel.java
@@ -1,3 +1,13 @@
+/*****************************************************************
+* The SearchPanel is a JPanel that is responsible for providing a
+* search bar that allows users to search for weather information
+* about specific cities. The SearchPanel has a JSONObject that
+* it uses to communicate with the Wunderground API when searching
+* for weather information. The SearchPanel knows how to instantiate
+* that JSONObject and instantiate itself.
+*
+* Kiran Ganeshan
+****************************************************************/
 import javax.swing.*;
 import java.awt.image.*;
 import java.awt.*;
@@ -5,6 +15,14 @@ import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
 public class SearchPanel extends JPanel{
-  public SearchPanel(){
-  }
+   private static JSONObject searchIndex;
+   /************************************************************* 
+   * Instantiates the JSONObject required to retain information
+   * from the Wunderground API
+   **************************************************************/
+   static{}
+   /************************************************************* 
+   * Instantiates the CurrentPanel object
+   **************************************************************/
+  public SearchPanel(){}
 }
