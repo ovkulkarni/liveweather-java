a815f238b0e3d836c18838a41f26973770dc7e22
kimcity <kiran@theganeshans.com>
2016-05-16 23:28:05 -0400

    Update SingleDayPanel.java

--- SingleDayPanel.java
+++ SingleDayPanel.java
@@ -4,9 +4,12 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
-public class SingleDayPanel extends JPanel
-{
-   public SingleDayPanel(){
-      
-   }
+public class SingleDayPanel extends JPanel {
+   private static JSONObject tenDay;
+   /************************************************************* 
+   * Instantiates the JSONObject required to retain information
+   * from the Wunderground API
+   **************************************************************/
+   static{}
+   public SingleDayPanel(){}
 }
