995dcc3955b9f589f7a75b3bc7e01fc7e6eafaca
kimcity <kiran@theganeshans.com>
2016-05-16 23:30:35 -0400

    Create SingleDayPanelStub.java

---
+++ stubs/SingleDayPanelStub.java
@@ -0,0 +1,26 @@
+/*****************************************************************
+* The SingleDayPanel is a JPanel that displays information from
+* a single day within a ten day forecast. The SingleDayPanel has
+* a JSONObject that it uses to retrieve information it needs to
+* display, and it knows how to instantiate that object and itself.
+*
+* Kiran Ganeshan
+****************************************************************/
+import javax.swing.*;
+import java.awt.image.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import org.json.*;
+import java.net.*;
+public class SingleDayPanel extends JPanel {
+   private static JSONObject tenDay;
+   /************************************************************* 
+   * Instantiates the JSONObject required to retain information
+   * from the Wunderground API
+   **************************************************************/
+   static{}
+   /************************************************************* 
+   * Instantiates the SingleDayPanel object
+   **************************************************************/
+   public SingleDayPanel(){}
+}
