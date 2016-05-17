7958901c26adf6078a55eb9ac2df0ac1fae64f58
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-05-16 22:23:11 -0400

    add stubs folder

--- Weather.java
+++ Weather.java
@@ -3,7 +3,13 @@ import java.io.*;
 import org.json.*;
 
 public class Weather{
-   
+
+   /**
+    * Returns a JSONObject from a given URL. 
+    *
+    * @param  link Link of the page with the JSON Data
+    * @return      The JSONObject generated from the URL
+    */
    public static JSONObject getJSONFromURL(String link) throws Exception{
       URL url = new URL(link);
       URLConnection connection = url.openConnection();
@@ -17,59 +23,71 @@ public class Weather{
       JSONObject results = new JSONObject(data.toString());
       return results;
    }
-      
+   
+   /**
+    * Returns a name of a city from a given query. 
+    *
+    * @param  query String with part of the name of the city; US ONLY
+    * @return       A String with the name of the Location
+    */
+    
    public static String getName(String query) throws Exception{
       JSONObject results = getJSONFromURL("http://autocomplete.wunderground.com/aq?query=" + URLEncoder.encode(query) + "&h=0&c=US");
       return results.getJSONArray("RESULTS").getJSONObject(0).getString("name");
    }
-
+   /**
+    * Returns a name of a city from a given query. 
+    *
+    * @param  query Get's special code from Wunderground to use in future requests from
+    * @return       A String with the special code
+    */
    public static String getEnding(String query) throws Exception {
       JSONObject results = getJSONFromURL("http://autocomplete.wunderground.com/aq?query=" + URLEncoder.encode(query) + "&h=0&c=US");
       return results.getJSONArray("RESULTS").getJSONObject(0).getString("l");
    }
 
    public static JSONObject getCurrentResults(String query) throws Exception{
-       return getJSONFromURL("http://api.wunderground.com/api/ccb47836f398476f/conditions" + getEnding(query) + ".json");
+      return getJSONFromURL("http://api.wunderground.com/api/ccb47836f398476f/conditions" + getEnding(query) + ".json");
    }
 
    public static JSONObject getCurrentConditions(String query) throws Exception{
-       return getCurrentResults(query).getJSONObject("current_observation");
+      return getCurrentResults(query).getJSONObject("current_observation");
    }
 
    public static String getTemperature(JSONObject data){
-        return data.getString("temperature_string");
+      return data.getString("temperature_string");
    }
 
    public static String getImageURL(JSONObject data){
-        return data.getString("icon_url");
-    }
+      return data.getString("icon_url");
+   }
 
    public static String getFullName(JSONObject data){
-       return data.getJSONObject("display_location").getString("full");
-    }
+      return data.getJSONObject("display_location").getString("full");
+   }
 
-    public static String getCondition(JSONObject data){
-        return data.getString("weather");
-    }
+   public static String getCondition(JSONObject data){
+      return data.getString("weather");
+   }
 
-    public static String getDetailedURL(JSONObject data){
-        return data.getString("forecast_url");
-    }
+   public static String getDetailedURL(JSONObject data){
+      return data.getString("forecast_url");
+   }
 
-    public static String getTimeAndDate(JSONObject data){
-        return data.getString("local_time_rfc822");
-    }
+   public static String getTimeAndDate(JSONObject data){
+      return data.getString("local_time_rfc822");
+   }
 
-    public static String getWindConditions(JSONObject data){
-        return data.getString("wind_string");
-    }
+   public static String getWindConditions(JSONObject data){
+      return data.getString("wind_string");
+   }
 
-    public static String getHumidity(JSONObject data){
-        return data.getString("relative_humidity");
-    }
+   public static String getHumidity(JSONObject data){
+      return data.getString("relative_humidity");
+   }
 
-    public static String getActualTemp(JSONObject data){
-        return data.getString("feelslike_string");
-    }
+   public static String getActualTemp(JSONObject data){
+      return data.getString("feelslike_string");
+   }
 
 }
