78f5f60610ba26be3186ce542bd558bf4447fb7f
kimcity <kiran@theganeshans.com>
2016-05-16 23:21:16 -0400

    Update AlertsPanel.java

--- AlertsPanel.java
+++ AlertsPanel.java
@@ -1,13 +1,28 @@
+/*****************************************************************
+* The AlertsPanel is a JPanel that displays any weather alerts
+* that were issued for an area. The AlertsPanel has a JSONObject
+* that it uses to retrieve information from Wunderground API, and
+* it knows how to instantiate that JSONObject and instantiate
+* itself.
+*
+* Kiran Ganeshan
+****************************************************************/
 import javax.swing.*;
 import java.awt.image.*;
 import java.awt.*;
 import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
-public class AlertsPanel extends JPanel 
-
-{
-      public AlertsPanel(){
-      }
+public class AlertsPanel extends JPanel {
+   private static JSONObject alerts;
+   /************************************************************* 
+   * Instantiates the JSONObject required to retain information
+   * from the Wunderground API
+   **************************************************************/
+   static{}
+   /************************************************************* 
+   * Instantiates the AlertsPanel
+   **************************************************************/
+   public AlertsPanel(){}
 }
 
