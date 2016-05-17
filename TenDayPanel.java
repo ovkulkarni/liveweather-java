d71a754733ce57166b36a833ca953c3988c2b646
kimcity <kiran@theganeshans.com>
2016-05-16 22:37:42 -0400

    Update TenDayPanel.java

--- TenDayPanel.java
+++ TenDayPanel.java
@@ -4,9 +4,28 @@ import java.awt.*;
 import javax.imageio.ImageIO;
 import org.json.*;
 import java.net.*;
+/*****************************************************************
+* The TenDayPanel is a JPanel that displays the most basic weather
+* information for the next ten days. TenDayPanel has a JSONObject
+* that it uses to retreive data from the Wunderground API, and
+* it also has an array of SingleDayPanels, each of which display
+* one day of weather. The TenDayPanel knows how to instantiate
+* it's JSONObject and how to instantiate itself.
+*
+* Nikki Prabhu
+****************************************************************/
 public class TenDayPanel extends JPanel 
 {
+   private static JSONObject tenDay;
    SingleDayPanel[] dayPanels = new SingleDayPanel[10];
+   /************************************************************* 
+   * Instantiates the JSONObject required to retain information
+   * from the Wunderground API
+   **************************************************************/
+   static{}
+   /************************************************************* 
+   * Instantiates a TenDayPanel object
+   **************************************************************/
    public TenDayPanel()
    {
       for(int x=0;x<10;x++)
