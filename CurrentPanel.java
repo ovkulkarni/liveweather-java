cf5b34644c5860866aa95368d734a3e4a26df688
kimcity <kiran@theganeshans.com>
2016-05-24 15:29:53 -0400

    Add files via upload
    formatted date and time string

--- CurrentPanel.java
+++ CurrentPanel.java
@@ -19,6 +19,7 @@ import java.net.*;
 public class CurrentPanel extends JPanel
 { 
    private static JSONObject current;
+   
    /************************************************************* 
    * Instantiates the JSONObject required to retain information
    * from the Wunderground API
@@ -36,14 +37,15 @@ public class CurrentPanel extends JPanel
    **************************************************************/
    public CurrentPanel()
    {
+      Font sample = new Font("Serif", Font.PLAIN, 20);
       try{
          setLayout(new GridLayout(6, 0));
-         addLocation(this);
-         addImage(this);
-         addDate(this);
-         addCondition(this);
-         addTemp(this);
-         
+         addLocation(this, sample);
+         addImage(this, sample);
+         addDate(this, sample);
+         addCondition(this, sample);
+         addTemp(this, sample);
+         addFeelsLike(this, sample);
       }
       catch(Exception e){
          System.out.println(e);
@@ -55,10 +57,11 @@ public class CurrentPanel extends JPanel
    * 
    * @param p  A Panel to add the image to ("this")
    **************************************************************/
-   public void addImage(JPanel p) throws Exception{
+   public void addImage(JPanel p, Font f) throws Exception{
       URL url = new URL(Weather.getImageURL(current));
       BufferedImage image = ImageIO.read(url);
       JLabel label = new JLabel(new ImageIcon(image));
+      label.setFont(f);
       p.add(label);
    }
    /************************************************************* 
@@ -66,8 +69,16 @@ public class CurrentPanel extends JPanel
    * 
    * @param p  A Panel to add the label to ("this")
    **************************************************************/
-   public void addDate(JPanel p) throws Exception{
+   public void addDate(JPanel p, Font f) throws Exception{
       JLabel label = new JLabel(Weather.getTimeAndDate(current));
+      String unformat = label.getText();
+      String wd = formatWeekday(unformat.substring(0,3));
+      String date = formatDate(unformat.substring(5,16));
+      //unformat = unformat.substring(16,unformat.length());
+      
+      String finalText = wd + ", " + date;
+      label.setText(finalText);
+      label.setFont(f);
       p.add(label);
    }
    /************************************************************* 
@@ -76,8 +87,9 @@ public class CurrentPanel extends JPanel
    * 
    * @param p  A Panel to add the label to ("this")
    **************************************************************/
-   public void addCondition(JPanel p) throws Exception{
+   public void addCondition(JPanel p, Font f) throws Exception{
       JLabel label = new JLabel(Weather.getCondition(current));
+      label.setFont(f);
       p.add(label);
    }
    /************************************************************* 
@@ -86,11 +98,15 @@ public class CurrentPanel extends JPanel
    * 
    * @param p  A Panel to add the labels to ("this")
    **************************************************************/
-   public void addTemp(JPanel p) throws Exception{
+   public void addTemp(JPanel p, Font f) throws Exception{
       JLabel label = new JLabel("Temperature: " + Weather.getTemperature(current));
-      JLabel label2 = new JLabel("Feels Like: " + Weather.getActualTemp(current));
+      label.setFont(f);
       p.add(label);
-       p.add(label2);
+   }
+   public void addFeelsLike(JPanel p, Font f) throws Exception{
+      JLabel label = new JLabel("Feels Like: " + Weather.getActualTemp(current));
+      label.setFont(f);
+      p.add(label);
    }
    /************************************************************* 
    * Adds a label showing the location from which the Wunderground
@@ -98,8 +114,46 @@ public class CurrentPanel extends JPanel
    * 
    * @param p  A Panel to add the label to ("this")
    **************************************************************/
-   public void addLocation(JPanel p) throws Exception{
+   public void addLocation(JPanel p, Font f) throws Exception{
       JLabel label = new JLabel(Weather.getFullName(current));
+      label.setFont(f);
       p.add(label);
+   }
+   /************************************************************* 
+   * Formats the weekday text received form Wunderground API
+   * 
+   * @param p  The weekday string given by Wunderground API
+   * @return   The formatted weekday
+   **************************************************************/
+   public String formatWeekday(String wd){
+      switch(wd){
+         case "Mon":wd="Monday";break;
+         case "Tue":wd="Tuesday";break;
+         case "Wed":wd="Wednesday";break;
+         case "Thu":wd="Thursday";break;
+         case "Fri":wd="Friday";break;
+         case "Sat":wd="Saturday";break;
+         case "Sun":wd="Sunday";break;
+      }
+      return wd;
+   }
+   /************************************************************* 
+   * Formats the date and time text received form Wunderground API
+   * 
+   * @param p  The date and time string given by Wunderground API
+   * @return   The formatted date
+   **************************************************************/
+   public String formatDate(String date){
+      String day = date.substring(0,date.indexOf(" "));
+      int daynum = Integer.parseInt(day);
+      if((daynum==1)||(daynum==21)||(daynum==31)){day=day+"st";}
+      else if((daynum==2)||(daynum==22)){day=day+"nd";}
+      else if((daynum==3)||(daynum==23)){day=day+"nd";}
+      else{day=day+"th";}
+      date = date.substring(date.indexOf(" ")+1,date.length());
+      String month = date.substring(0,date.indexOf(" "));
+      date = date.substring(date.indexOf(" ")+1,date.length());
+      String year = date;
+      return month + " " + day + ", " + year;
    }
 }
