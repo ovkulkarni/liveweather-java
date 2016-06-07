55e9e2ca2d8860b04bf71066ae819c9fdcb9fbd3
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-06-06 23:35:00 -0400

    oops

--- Driver.java
+++ Driver.java
@@ -12,15 +12,9 @@ public class Driver {
    **************************************************************/
    public static void main(String[] args) throws Exception{
       JFrame frame = new JFrame("Live Weather");
-<<<<<<< HEAD
-      frame.setSize(900,750);
-      frame.setLocation(50,50);
-      frame.setResizable(true);
-=======
       frame.setSize(955, 900);
       frame.setLocation(0,0);
       frame.setResizable(false);
->>>>>>> origin/master
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.getContentPane().add(new Panel());
       frame.setVisible(true);
